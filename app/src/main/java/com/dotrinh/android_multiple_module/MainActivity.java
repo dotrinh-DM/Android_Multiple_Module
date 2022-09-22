37b982ec203b5025fd26fdbf5e0715a79746c4e0
Do Trinh <thanhdo.trinh@gmail.com>
2022-09-22 21:53:43 +0900

    basic proj

---
+++ app/src/main/java/com/dotrinh/android_multiple_module/MainActivity.java
@@ -0,0 +1,19 @@
+/*
+ * Created by dotrinh on 9/22/22, 9:53 PM
+ * Copyright (c) 2022. dotr Inc. All rights reserved.
+ */
+
+package com.dotrinh.android_multiple_module;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
